6fa3e2d6910f444cb6ae82e4e11416036850c03d
unknown <you@example.com>
2026-09-03 06:53:10 +0530

    server-client-demo

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,29 @@
-export default function Home(){
+import Counter from './components/counter'
+import Link from 'next/link';
+
+export default async function Home(){
+
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+
+    console.log("Data = ",data);
+
     return(
         <div>
             <h1>Home Page</h1>
-            <p>Welcome to Home page !</p>
+
+            <Link href='/server-client-demo'>get-server-client-demo</Link>
+
+            {/* {
+                data.map(item => (
+                    <li key={item.id}>
+                        <h2>{item.title}</h2>
+                        <p>{item.body}</p>
+                    </li>
+                    
+                ))
+            } */}
+            <Counter/>
         </div>
     )
 }
---
+++ app/server-client-demo/page.tsx
@@ -0,0 +1,12 @@
+import Counter from "../components/counter";
+import ServerInfo from '../components/ServerInfo';
+export default function ServerClientDemo(){
+    return(
+
+        <main>
+            <h1>Server Demo</h1>
+            <ServerInfo/>
+            <Counter/>
+        </main>
+    )
+}
