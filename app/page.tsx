3f8ab93a77823d5c8e01b7b1efa0c849e9291ccf
unknown <you@example.com>
2026-09-03 07:23:16 +0530

    Home done

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,9 @@
-interface Post{
-    id:number,
-    title:string,
-    body:string
-}
 
 export default async function Home(){
-
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts',{
-        cache:"no-store"  // no-store dynamic fatching any change in api chnge in ui
-    })
-
-    const posts: Post[] = await res.json();
-
     return(
         <div>
-            <h1>Posts Fetch Data by server</h1> 
-            <ul>
-                <h1>Static Rendring</h1>
-                {
-                    posts.slice(0,5).map(post =>(
-                        <li key={post.id}>
-                            <h1>{post.title}</h1>
-                            <p>{post.body}</p>
-                        </li>
-                    ))
-                }
-            </ul>
-            <h1>Dynamic rendring</h1>
-            <pre>{JSON.stringify(posts.slice(0,2),null,2)}</pre>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the main landing page of the application</p>
         </div>
     )
 }
